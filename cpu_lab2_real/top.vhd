14d8c66ceb26b2e4cee5e741ce78d2ef785cfa8c
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-04 21:30:17 +0300

    all 0

--- cpu_lab2_real/top.vhd
+++ cpu_lab2_real/top.vhd
@@ -11,46 +11,49 @@ entity top is
 end top;
 ------------------------------------------------------------------
 architecture arc_sys of top is
-    signal counter:std_logic_vector(n-1 downto 0);
-    signal restart, reset_bound:std_logic;
+    signal counter_reg:std_logic_vector(n-1 downto 0);
+    signal restart:std_logic;
 	
 	
 begin
 	--------------------------------------------------------------
 	proc1 : process(clk,rst)
+        variable counter :std_logic_vector(n-1 downto 0):=(others=>'0');
 	begin
         --counter <=(others =>'0');
         --restart <='1';
 		if (rst ='1') then
-            counter <=(others =>'0'); 
+            counter :=(others =>'0'); 
+            restart<= '1';
         elsif (clk'event and clk='1') then
             if (restart = '1') then 
-                counter <= (others => '0');
-                restart <= '0';
+                counter := (others => '0');
             else 
-				counter <= counter +1;
+				counter := counter +1;
             end if;
         end if;
-        countOut<= counter;
+        --countOut<= counter;
+        counter_reg <=counter;
 	end process;
 	--------------------------------------------------------------
 	proc2 : process(clk,rst)
         variable bound :std_logic_vector(n-1 downto 0):=(others=>'0');
-    begin
-        --if (reset_bound ='1') then
-        --    bound :=(others=>'0');
-        if (clk'event and clk='0') then
-            if (counter = bound) then
+begin
+        if (rst ='1') then
+            bound :=(others =>'0'); 
+        elsif (clk'event and clk='0') then
+            if (counter_reg = bound) then
 				bound := bound+1;
                 restart <='1';
-				if (bound >upperBound) then
-                    --reset_bound <='1';
+				if (bound =upperBound) then
+                    bound :=(others=>'0');
+                    
                 end if;
             end if; 
         end if;
 	end process;
 	--------------------------------------------------------------
-	
+	countOut <= counter_reg;
 	
 	
 	

7b303c1822037e82e815ccc978c4dfd9c90e2ebd
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-06 07:52:41 +0300

    modifed double assignment into single assigment for bound

--- cpu_lab2_real/top.vhd
+++ cpu_lab2_real/top.vhd
@@ -11,17 +11,13 @@ entity top is
 end top;
 ------------------------------------------------------------------
 architecture arc_sys of top is
-    --signal counter_reg:std_logic_vector(n-1 downto 0);
     signal restart:std_logic;
 	signal counter,bound :std_logic_vector(n-1 downto 0);--:=(others=>'0');
 	
 begin
 	--------------------------------------------------------------
 	proc1 : process(clk,rst)
-        --variable counter :std_logic_vector(n-1 downto 0);--:=(others=>'0');
 	begin
-        --counter <=(others =>'0');
-        --restart <='1';
 		if (rst ='1') then
             counter <=(others =>'0'); 
             restart<= '1';
@@ -34,23 +30,20 @@ begin
 				counter <= counter +1;
             end if;
         end if;
-        --countOut<= counter;
-        --counter_reg <=counter;
 	end process;
 	--------------------------------------------------------------
 	proc2 : process(clk,rst)
-        --variable bound :std_logic_vector(n-1 downto 0);--:=(others=>'0');
 begin
         --if (rst ='1') then
             --bound <=(others =>'0');
             --restart<='1';
         if (clk'event and clk='0') then
-            if (counter = bound) then
+            if (bound >=upperBound) then
+                bound <=(others=>'0');
+                restart<='1';
+            elsif (counter = bound) then
 				bound <= bound+1;
                 restart <='1';
-				if (bound >=upperBound) then
-                    bound <=(others=>'0'); 
-                end if;
             end if; 
         end if;
 	end process;

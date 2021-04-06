04b15926951f35577ae6392191b6671214029755
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-06 14:39:23 +0300

    add two filse for test benches

---
+++ cpu_lab2_real/tb_2.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+    use IEEE.std_logic_1164.all;
+    use IEEE.numeric_std.all;
+
+entity tb_2 is
+    port (
+        clk: in  std_logic;
+        rst: in  std_logic
+    );
+end entity;
+
+architecture rtl of tb_2 is
+begin
+end architecture;

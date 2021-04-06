1ac1ea9b7e01a28557563f97299e0a34c2b7d7ff
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-06 12:39:08 +0300

    add bound_check on tb

--- cpu_lab2_real/top.vhd
+++ cpu_lab2_real/top.vhd
@@ -1,3 +1,6 @@
+--VHDL program that implement a counter with Bound.
+--Bound starts from zero and increase every time counter reach bound.
+--Bound reset every time he reaches upperBound.
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
@@ -7,44 +10,43 @@ entity top is
 	generic ( n : positive := 8 ); 
 	port( rst,clk : in std_logic;
 		  upperBound : in std_logic_vector(n-1 downto 0);
-		  countOut : out std_logic_vector(n-1 downto 0));
+		  kfir_bound,countOut : out std_logic_vector(n-1 downto 0));
+		
 end top;
 ------------------------------------------------------------------
 architecture arc_sys of top is
-	signal counter,bound :std_logic_vector(n-1 downto 0);--:=(others=>'0');
-	signal increment : std_logic;
+	signal counter,bound :std_logic_vector(n-1 downto 0);
+	signal enable_inc : std_logic; -- enable increment of bound
 begin
-	--------------------------------------------------------------
-	proc1 : process(clk,rst)
-	begin
-		if (rst ='1') then
-            counter <=(others =>'0'); 
-        elsif (clk'event and clk='1') then
-            if (counter >= bound) then
-                counter <= (others => '0');
-                increment <='1';
-            elsif (counter< bound) then
-				counter <= counter +1;
-            end if;
-        end if;
-	end process;
-	--------------------------------------------------------------
-	proc2 : process(clk,rst)  
+------------------------------------------------------------------
+proc1 : process(clk,rst)
 begin
-        if (rst ='1') then
-            bound <=(others =>'0');
-        elsif (clk'event and clk='0') then
-            if (bound>upperBound) then
-                bound <=(others=>'0');
-                elsif (increment='1')and(counter = 0) then
-                bound <= bound+1;
-            end if; 
+	if (rst ='1') then
+		counter <=(others =>'0'); 
+    elsif (clk'event and clk='1') then -- rising edge
+		if (counter >= bound) then
+			counter <= (others => '0');
+            enable_inc <='1';
+        elsif (counter< bound) then
+			counter <= counter +1;
         end if;
-	end process;
-	--------------------------------------------------------------
-	countOut <= counter;
-	
-	
-	
-	
+	end if;
+end process;
+------------------------------------------------------------------
+proc2 : process(clk,rst)  
+begin
+	if (rst ='1') then
+		bound <=(others =>'0');
+    elsif (clk'event and clk='0') then -- falling edge
+		if (bound>upperBound) then
+			bound <=(others=>'0');
+		elsif (enable_inc='1')and(counter = 0) then
+			bound <= bound+1;
+        end if; 
+    end if;
+end process;
+------------------------------------------------------------------
+countOut <= counter;
+kfir_bound <=bound;
+
 end arc_sys;
